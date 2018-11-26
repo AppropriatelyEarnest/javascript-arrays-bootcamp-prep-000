b62b1c27e55232509ab5e90d49af9ec020e78706
ruthnewman <ruthnewman@gmail.com>
2018-11-26 00:06:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,6 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var element = 'foo';
+
 
 function addElementToBeginningOfArray(array, element) {
  return [element, ...array];
